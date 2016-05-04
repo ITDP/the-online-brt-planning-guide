e3b6ba83ae733fea6d5ecec68ef19dd84ae30f36
Jonas Malaco Filho <jonas@jonasmalaco.com>
2016-05-04 17:48:38 -0300

    Parser: [emphasis][highlight] test nested cmds

--- generator/test/ParserTests.hx
+++ generator/test/ParserTests.hx
@@ -83,6 +83,9 @@ class ParserTests {
 		Assert.same(
 			expand(Paragraph(@wrap(6,1)Emphasis(HList([@len(1)Word("a"),@len(1)Wordspace,@len(1)Word("b")])))),
 			parse("\\emph{a b}"));
+		Assert.same(
+			expand(Paragraph(@wrap(6,1)Emphasis(HList([@len(1)Word("a"),@len(1)Wordspace,@wrap(6,1)Emphasis(@len(1)Word("b"))])))),
+			parse("\\emph{a \\emph{b}}"));
 
 		Assert.same(
 			expand(Paragraph(@wrap(1,1)Emphasis(@len(1)Word("a")))),
@@ -109,6 +112,9 @@ class ParserTests {
 		Assert.same(
 			expand(Paragraph(@wrap(11,1)Highlight(HList([@len(1)Word("a"),@len(1)Wordspace,@len(1)Word("b")])))),
 			parse("\\highlight{a b}"));
+		Assert.same(
+			expand(Paragraph(@wrap(11,1)Highlight(HList([@len(1)Word("a"),@len(1)Wordspace,@wrap(11,1)Highlight(@len(1)Word("b"))])))),
+			parse("\\highlight{a \\highlight{b}}"));
 	}
 
 	public function test_005_bad_command_name()
