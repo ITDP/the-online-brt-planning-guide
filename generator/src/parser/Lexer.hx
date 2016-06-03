675e184e1df45f0316f1016ae4ff5012175d09c7
Jonas Malaco <jonas@protocubo.io>
2016-06-02 21:32:46 -0300

    Gen: prevent stack overflows when lexing math

--- generator/src/parser/Lexer.hx
+++ generator/src/parser/Lexer.hx
@@ -45,7 +45,6 @@ class Lexer extends hxparse.Lexer implements hxparse.RuleBuilder {
 		}
 	];
 
-
 	static var math = @:rule
 	[
 		"$" => checkExpr(),
@@ -53,7 +52,7 @@ class Lexer extends hxparse.Lexer implements hxparse.RuleBuilder {
 			buf.add(lexer.current);
 			lexer.token(math);
 		},
-		"[^$]" =>
+		"[^$]+" =>
 		{
 			buf.add(lexer.current);
 			lexer.token(math);
