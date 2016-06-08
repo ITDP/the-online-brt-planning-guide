e50cde9d59cd34761a77e46fd1e25c519f572264
Jonas Malaco <jonas@protocubo.io>
2016-06-08 18:32:27 -0300

    Gen: [tex] proof of concept table generation
     - large table (margin + text) only; need commands to distingish all three
       types and the small size won't use the automatic column width
       computation in Haxe
     - absolute dimensions; need to to make everything related to the table
       module size
     - NO_MODULES is fixed; this needs to accomodate both medium (text only)
       and large (text + margin) tables
     - hard-coded styling; need to use macros to be defined externally in
       the preamble.tex
     - missing style options for first row and both vertical and horizontal
       lines

--- generator/src/generator/TexGen.hx
+++ generator/src/generator/TexGen.hx
@@ -50,6 +50,144 @@ class TexGen {
 		}
 	}
 
+	static inline var CHAR_COST = 1;
+	static inline var SPACE_COST = 1;
+	static inline var PAR_BREAK_COST = 10;
+	static inline var LINE_BREAK_COST = 10;
+	static inline var BULLET_COST = 1;
+	static inline var FIG_MARK_COST = 10;
+	static inline var TBL_MARK_COST = 10;
+	static inline var BAD_COST = 1000;
+	static inline var QUOTE_COST = 1;
+	static inline var EM_DASH_COST = 2;
+	static inline var NO_MODULES = 30;
+	static inline var MIN_COLUMN = 4;
+	static inline var SEPAR_SIZE = 1;
+
+	function pseudoHTypeset(h:HElem)
+	{
+		return switch h.def {
+		case Wordspace: SPACE_COST;
+		case Emphasis(i), Highlight(i): pseudoHTypeset(i);
+		case Word(w): w.length;
+		case HList(li):
+			var cnt = 0;
+			for (i in li)
+				cnt += pseudoHTypeset(i);
+			cnt;
+		}
+	}
+
+	function pseudoTypeset(v:TElem)
+	{
+		return switch v.def {
+		case TLaTeXPreamble(_), THtmlApply(_): 0;
+		case TVolume(_), TChapter(_), TSection(_), TSubSection(_), TSubSubSection(_): BAD_COST; // not allowed in tables
+		case TVList(li):
+			var cnt = 0;
+			for (i in li) {
+				cnt += pseudoTypeset(i);
+				if (cnt > 0)
+					cnt += PAR_BREAK_COST;
+			}
+			cnt;
+		case TFigure(_, caption, cright, _): TBL_MARK_COST + pseudoHTypeset(caption) + SPACE_COST + pseudoHTypeset(cright);
+		case TTable(_), TBox(_): BAD_COST; // not allowed (for now?)
+		case TQuotation(text, by): QUOTE_COST + pseudoHTypeset(text) + QUOTE_COST + LINE_BREAK_COST + EM_DASH_COST + pseudoHTypeset(by);
+		case TList(li):
+			var cnt = 0;
+			for (i in li) {
+				cnt += BULLET_COST + SPACE_COST + pseudoTypeset(i);
+				if (cnt > 0)
+					cnt += LINE_BREAK_COST;
+			}
+			cnt;
+		case TParagraph(h): pseudoHTypeset(h);
+		}
+	}
+
+	// TODO document the objective and the implementation
+	function computeTableWidths(header, rows:Array<Array<TElem>>)
+	{
+		var width = header.length;
+		var cost = header.map(pseudoTypeset);
+		for (i in 0...rows.length) {
+			var r = rows[i];
+			if (r.length != width) continue;  // FIXME
+			for (j in 0...width) {
+				var c = r[j];
+				cost[j] += pseudoTypeset(c);
+			}
+		}
+		var tcost = Lambda.fold(cost, function (p,x) return p+x, 0);
+		var available = NO_MODULES - (width - 1)*SEPAR_SIZE;
+		var ncost = cost.map(function (x) return available/tcost*x);
+		for (i in 0...width) {
+			if (ncost[i] < MIN_COLUMN)
+				ncost[i] = MIN_COLUMN;
+		}
+		var icost = ncost.map(Math.round);
+		var miss = available - Lambda.fold(icost, function (p,x) return p+x, 0);
+		var priori = [for (i in 0...width) i];
+		var diff = [for (i in 0...width) Math.abs(ncost[i] - icost[i])];
+		priori.sort(function (a,b) return Reflect.compare(diff[b], diff[a]));
+		var itCnt = 0;
+		while (miss != 0 && itCnt++ < 4) {
+			for (p in 0...width) {
+				var i = priori[p];
+				if (diff[i] == 0) continue;
+				if (miss > 0) {
+					icost[i]++;
+					miss--;
+				} else if (miss < 0) {
+					if (icost[i] - 1 < MIN_COLUMN) continue;
+					icost[i]--;
+					miss++;
+				} else {
+					break;
+				}
+			}
+		}
+		var check = available - Lambda.fold(icost, function (p,x) return p+x, 0);
+		assert(check == 0 && Lambda.foreach(icost, function (x) return x >= MIN_COLUMN), check, width, ncost, icost, priori, itCnt);
+		return icost;
+	}
+
+	function genTable(genAt, pos, caption, header:Array<TElem>, rows:Array<Array<TElem>>, count, id)
+	{
+		var colWidths = computeTableWidths(header, rows);
+		var buf = new StringBuf();
+		buf.add('% FIXME\nTable ${genh(caption)}:\n\n');
+		var width = header.length;
+		buf.add("\\halign to 154mm{\\kern -49mm\n\t\\hfill");
+		var t = 154/105*1.75;
+		for (i in 0...width) {
+			if (i > 0)
+				buf.add('\\hbox to ${t}mm{}&\\hbox to ${t}mm{}');
+			var size = colWidths[i]*154/105*3.5;
+			buf.add('\\vtop{\\sffamily\\footnotesize\\noindent\\hsize=${size}mm#}');
+		}
+		buf.add("\\cr\n\t");
+		function genCell(i:TElem) {
+			return switch i.def {
+			case TParagraph(h): genh(h);
+			case _: genv(i, genAt);
+			}
+		}
+		buf.add(header.map(genCell).join("&"));
+		buf.add("\\cr\n");
+		for (r in rows) {
+			weakAssert(r.length == width, header.length, r.length);
+			buf.add("\t");
+			if (r.length != width)
+				buf.add("% ");  // FIXME
+			buf.add(r.map(genCell).join("&"));
+			buf.add("\\cr\n");
+		}
+		buf.add('}\n${genp(pos)}\n');
+		return buf.toString();
+	}
+
 	function genv(v:TElem, at:String)
 	{
 		assert(!at.endsWith(".tex"), at, "should not but a directory");
@@ -112,29 +250,7 @@ class TexGen {
 		case THtmlApply(_):
 			return "";
 		case TTable(caption, header, chd, count, id):
-			var buf = new StringBuf();
-			buf.add('% FIXME\nTable ${genh(caption)}:\n\n');
-			var width = header.length;
-			buf.add("\\halign{\n\t\\beforefirstcell");
-			for (i in 1...width)
-				buf.add("#\\aftercell&\\beforecell");
-			buf.add("#\\afterlastcell\\cr\n\t");
-			function genCell(i:TElem) {
-				return switch i.def {
-				case TParagraph(h): genh(h);
-				case _: genv(i, at);
-				}
-			}
-			buf.add(header.map(genCell).join("&"));
-			buf.add("\\cr\n");
-			for (r in chd) {
-				assert(r.length == width, header.length, r.length);
-				buf.add("\t");
-				buf.add(r.map(genCell).join("&"));
-				buf.add("\\cr\n");
-			}
-			buf.add('}\n${genp(v.pos)}\n');
-			return buf.toString();
+			return genTable(at, v.pos, caption, header, chd, count, id);
 		}
 	}
 
