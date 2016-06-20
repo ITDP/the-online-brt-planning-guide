935e080dbcb666dedcfc5c076a6e09d0d2ecbf5b
Jonas Malaco <jonas@protocubo.io>
2016-06-20 19:36:31 -0300

    Gen: [html] temporary italic on box names

--- generator/src/generator/HtmlGen.hx
+++ generator/src/generator/HtmlGen.hx
@@ -113,7 +113,7 @@ class HtmlGen {
 			findSize(contents);
 			var b = new StringBuf();
 			b.add('<section class="box ${isLarge ? "lg" : "md"}">\n');
-			b.add('<h1>Box ${counts[CHA]}.${count}<span class="quad"></span>${horizontal(name)}</h1>\n');
+			b.add('<h1>Box ${counts[CHA]}.${count}<span class="quad"></span><em>${horizontal(name)}</em></h1>\n');
 			b.add(vertical(contents, counts, curNav));
 			b.add('</section>\n');
 			return b.toString();
