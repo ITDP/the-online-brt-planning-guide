fb1b38459e3c8e4a80eba062560110c7f3b398de
Jonas Malaco <jonas@protocubo.io>
2016-08-03 18:34:09 -0300

    Gen: [nt][html] partial nav js implementation

--- generator/src/html/script/Nav.hx
+++ generator/src/html/script/Nav.hx
@@ -6,10 +6,42 @@ import Assertion.*;
 import js.Browser.*;
 import js.jquery.Helper.*;
 
+using StringTools;
+
 class Nav {
+	static var data(get,never):String;
+		static function get_data() return js.Lib.global.__navBundle__;
+
 	static function drawNav(e:Event)
 	{
-		show("hi");
+		// parse and locate myselft
+		assert(data != null, "no data bundled");
+		var nav = J(JQuery.parseHTML(data));
+		assert(document.URL.startsWith(document.baseURI));
+		var myUrl = document.URL.substr(document.baseURI.length);
+		var me = nav.find('a[href="$myUrl"]').parent();
+		assert(me.length > 0);
+		assert(me.is("li"));
+		assert(me.hasClass("volume") || me.hasClass("chapter") || me.hasClass("section"), "classes used in selectors");
+
+		// remove grandchildren
+		me.children("ul").find("ul").remove();
+		assert(nav.find('a[href="$myUrl"]').parent().children("ul").find("ul").length == 0);
+
+		// remove distant ancestors
+		if (me.hasClass("volume")) {
+			nav.find("li.chapter").not(me.find("li.chapter")).remove();
+		} else if (me.hasClass("chapter")) {
+			nav.find("li.chapter").not(me).not(me.siblings("li.chapter")).remove();
+			nav.find("li.section").not(me.find("li.section")).remove();
+		} else if (me.hasClass("section")) {
+			nav.find("li.chapter").not(me.parents("li.chapter")).remove();
+			nav.find("li.section").not(me).not(me.siblings("li.section")).remove();
+			me.siblings("li.section").find("li").not(me.find("li")).remove();
+		}
+
+		// insert
+		JTHIS.find("div.container").append(nav);
 	}
 
 	static function main()
