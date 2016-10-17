86cb8ede1018c3c6b2cbf3b0ada4ddb3daca1790
Jonas Malaco <jonas@protocubo.io>
2016-10-17 18:54:24 -0200

    Lfs: make the output even prettier

--- .enforce_lfs.sh
+++ .enforce_lfs.sh
@@ -7,9 +7,10 @@ function check_for_lfs_files_on_working_tree()
 	ret=$ok
 	for ext in $exts
 	do
-		find . -name \*.$ext -type f -exec file {} \; | grep -v 'ASCII text' \
-			&& echo "[31mERROR: rogue $ext files found[0m" && ret=$fail \
-			|| echo "Ok: no rogue $ext files found"
+		find . -name \*.$ext -type f -exec file {} \; | (grep -v 'ASCII text' > .rogue_files) \
+			&& echo "[31m  -> ERROR: rogue $ext files found:[0m" && ret=$fail \
+			&& cat .rogue_files | sed -e 's/^/[31m     /' | sed -e 's/$/[0m/' \
+			|| echo "  -> Ok: no rogue $ext files found"
 	done
 	return $ret
 }
@@ -24,21 +25,20 @@ fi
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
-	echo "[34mLooking at the merged working tree[0m"
+	echo "[34m==> Looking at the merged working tree[0m"
 	check_for_lfs_files_on_working_tree
 	status=$?
 else
 	status=$ok
 fi
 
-echo "Looking at commits"
+echo "Looking at commits (in reverse chronological order):"
 echo $commits
-echo "(in reverse chrnological order)"
 # TODO fetch all commits or handle missing commits from initial shallow clone
 
 for rev in $commits
 do
-	echo "[34mChecking commit $rev[0m"
+	echo "[34m==> Checking commit $rev[0m"
 	git checkout --quiet $rev
 	check_for_lfs_files_on_working_tree
 	if [ "$?" -ne "$ok" ]
