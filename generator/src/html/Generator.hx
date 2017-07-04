f4c5fbb52305d637d0e40d452b7000340eb343bb
Rafael Craice <craice@gmail.com>
2017-07-04 18:16:35 -0300

    CSS: navigation (generator)

--- generator/src/html/Generator.hx
+++ generator/src/html/Generator.hx
@@ -174,7 +174,7 @@ class Generator {
 		assert(!bufs.exists(path) || bufs[path] == reserved, bufs[path].toString());
 		bufs[path] = reserved;
 	}
-	
+
 	function unreserveBuffer(url:String)
 	{
 		var path = urlToPath(url);
@@ -536,9 +536,10 @@ class Generator {
 				continue;
 
 			if (p.endsWith(".html")) {
-                b.add('<div class="prev"><span id="prev-loading">previous page...</span></div>\n'); 
-                b.add('<div class="next"><span id="next-loading">next page...</span></div>\n'); 
 				b.add("<div class="navigation">\n");
+                b.add('<div class="prev"><span id="prev-loading">previous page...</span></div>\n');
+                b.add('<div class="next"><span id="next-loading">next page...</span></div>\n');
+				b.add("</div>\n");
 				b.add("</div>\n");
 				b.add("</div>\n");
 				b.add('<script src="$script"></script>');
