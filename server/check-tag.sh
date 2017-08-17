733069604cf47fe7af70263d1c0ec9e6dc9e49a2
lehonma <caio.honma@gmail.com>
2017-08-17 14:41:11 -0300

    Prob. fix for tag

--- server/check-tag.sh
+++ server/check-tag.sh
@@ -3,13 +3,13 @@ minor=0
 
 #Checks if $0 is greater than cur major/minor
 function compare_versions()
-{
+{	
 	#eliminate "v" char; on zsh string index starts @1!
 	local cur_substr=${1:1}
 	local cur_major=${cur_substr%.*}
 	local cur_minor=${cur_substr#*.}
 	local ret=""
-
+	
 	if [[ cur_major -gt major ]]
 		then major=$cur_major ; minor=$cur_minor
 	elif [[ cur_major -eq major ]] && [[ cur_minor -gt minor ]]
@@ -19,7 +19,7 @@ function compare_versions()
 	return 0	
 }
 
-if [[ $IS_PR -eq 0 ]] 
+if [[ $IsPullRequest -eq "0" ]] 
 	then for t in $(git tag --list 'v*.*') 
 	do 
 	if [[ "$t" =~ ^[v\d+\.\d+]  ]]
@@ -27,5 +27,6 @@ if [[ $IS_PR -eq 0 ]]
 	fi
 	done
 fi
- 
-export TAG=$major.$minor
+
+export TAG=v$major.$minor
+echo $TAG
